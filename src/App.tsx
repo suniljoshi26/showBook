c5f27dd9c1f9561425038485fd431b0b61ad13d9
Sunil Joshi <joshisunil2601@gmail.com>
2023-01-05 10:52:33 +0530

    redux toll

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import ShowList from "./components/ShowList";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="text-red-500"> hi lucky</div>;
+  return (
+    <div className="text-red-500">
+      {" "}
+      <ShowList />
+    </div>
+  );
 }
 
 export default App;
